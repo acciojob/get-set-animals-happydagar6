7baae30c338184a450fbd8367087ec49247adcae
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-12 18:04:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    // Use a backing property (conventionally starting with _) 
+    // to avoid infinite recursion with the getter
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    // Access the species via 'this.species'
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
